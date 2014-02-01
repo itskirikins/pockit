4704f9894a1adfc856d87c865add59553821039a
Annie <annie@Annies-MacBook-Pro-2.local>
2014-02-01 14:56:59 -0500

    derp

--- src/com/pockit/pockit1/HomeActivity.java
+++ src/com/pockit/pockit1/HomeActivity.java
@@ -116,7 +116,7 @@ public class HomeActivity extends FragmentActivity implements
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.home, menu);
-		return true;
+		return super.onCreateOptionsMenu(menu);
 	}
 
 	@Override
