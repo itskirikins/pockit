2bae43d38b413badae87994322fc56c3fb6e9be6
Annie <annie@Annies-MacBook-Pro-2.local>
2014-02-01 13:40:47 -0500

    adding old stuffs

--- gen/com/pockit/pockit1/R.java
+++ gen/com/pockit/pockit1/R.java
@@ -22,11 +22,14 @@ public final class R {
     }
     public static final class drawable {
         public static final int bgtile=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int pic1=0x7f020002;
-        public static final int pic2=0x7f020003;
-        public static final int pockit_jump=0x7f020004;
-        public static final int stripe=0x7f020005;
+        public static final int event=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int importschedule=0x7f020003;
+        public static final int pic1=0x7f020004;
+        public static final int pic2=0x7f020005;
+        public static final int pockit_jump=0x7f020006;
+        public static final int stripe=0x7f020007;
+        public static final int tagline=0x7f020008;
     }
     public static final class id {
         public static final int action_settings=0x7f080006;
